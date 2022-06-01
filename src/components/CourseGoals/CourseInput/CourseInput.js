50b45295aa96fce53587ea5a8ed18e65a0b742ed
triple486 <triple486@gmail.com>
2022-06-01 19:31:10 +0900

    test prettier

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,22 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
   const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
-    if(event.target.value.trim().length > 0){
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
       setIsValid(true);
     }
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
-    if(enteredValue.trim().length === 0){
+    if (enteredValue.trim().length === 0) {
       setIsValid(false);
       return;
     }
@@ -26,14 +26,14 @@ const CourseInput = props => {
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
-        <input 
-          style={{ 
-            borderColor: !isValid ? 'red' : '#ccc', 
-            background: !isValid ? 'salmon' : 'transparent' 
-          }} 
-          type="text" 
-          onChange={goalInputChangeHandler} 
+        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
+        <input
+          style={{
+            borderColor: !isValid ? "red" : "#ccc",
+            background: !isValid ? "salmon" : "transparent",
+          }}
+          type="text"
+          onChange={goalInputChangeHandler}
         />
       </div>
       <Button type="submit">Add Goal</Button>
